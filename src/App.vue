1d5004121c3efb8936d7401fa47778a86e0df40c
JustDoItAgain99 <2540873503@qq.com>
2020-06-30 17:32:11 +0800

    第一天项目的初始环境配置完成

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    有没有啊
+    <div class="box"></div>
+    <!-- 注释的为全局导入 vantage组件 -->
+    <!-- <van-button type="primary">主要按钮</van-button>
+    <van-cell-group class="color">
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>-->
+    <van-button type="info">按钮</van-button>
+    <hr />User:
+    <van-field placeholder="请输入用户名" />
+  </div>
+</template>
+<script>
+</script>
+<style>
+/* .color {
+  border: 1px solid red;
+} */
+.app {
+  width: 300px;
+  height: 300px;
+  background-color: pink;
+}
+.app .box {
+  width: 150px;
+  height: 150px;
+  background-color: green;
+  font-size: 24px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import 'amfe-flexible'
+// import Vant from 'vant'
+// import 'vant/lib/index.css'  这个是全局导入用的
+// Vue.use(Vant)
+import { Button, Field } from 'vant'
 
+Vue.use(Button)
+Vue.use(Field)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
